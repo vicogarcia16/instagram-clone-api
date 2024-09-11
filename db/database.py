afc220c331177939b5d31adf9ec153ec07cf45c1
vmgn16 <mg_619@hotmail.com>
2024-09-11 00:28:58 -0600

    cambio de conexion bd + libreria

--- db/database.py
+++ db/database.py
@@ -1,10 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+if not os.getenv('PRODUCTION'):
+  from dotenv import load_dotenv
+  load_dotenv() 
  
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./ig_api.db'
+SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
  
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
  
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
  
