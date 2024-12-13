7dfc95e554e5119682d32864985a206ec082b5f8
vmgn16 <mg_619@hotmail.com>
2024-12-13 16:46:58 -0600

    agregar servidores swagger

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ app = FastAPI(
     description="This is a REST API for an Instagram Clone",
     version="0.1.1",
     docs_url="/",
-    redoc_url="/redoc"
+    redoc_url="/redoc",
+    servers=[{"url": os.getenv("SERVER_URL", "http://localhost:8000")}],
+    
 )
 
 app.include_router(authentication.router)
